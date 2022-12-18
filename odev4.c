5ba587868b0996f09066fe2dfac377d92316622f
yz-baskoy <yziyabaskoy@gmail.com>
2022-12-19 01:29:12 +0300

    Add documentation to functions

--- odev4.c
+++ odev4.c
@@ -35,7 +35,7 @@ int* inDegrees(graph* G);
 int inDegree(graph* G, int u);
 void printInfluencers(graph *g, int x, int y, Person * p);
 int calculateIndegreeConnections(graph* graph, int start, int end);
-int calculateIndegreeConnectionsBFS(graph* graph, int node);
+int calculateTotalConnectionsBFS(graph* graph, int node);
 void freePeople(Person* people, int num_people);
 Person* parseData(const char* data, int* num_people);
 void printPersonNameById(int id, Person *people, int numPeople); 
@@ -55,7 +55,7 @@ int main() {
     data[data_len] = '\0';
 
     // Parse the data and create the list of Person objects
-    int num_people;
+    int num_people, X, Y, M;
     Person* people = parseData(data, &num_people);
     free(data);
 
@@ -67,12 +67,19 @@ int main() {
         Person person = people[i];
         printf("indis: %d, ID: %d, Name: %s %s, Connections:", i, person.id, person.name, person.last_name);
         for (int j = 0; j < person.num_connections; j++) {
-            printf("addEdge(%d %d)",person.id, person.connections[j]);
+            printf("%d %d",person.id, person.connections[j]);
 			addEdge(g1, person.id, person.connections[j]);
         }
         printf("\n");
     }
 	printGraph(g1);
+
+    printf("M degerini giriniz:\n");
+    scanf("%d", &M);
+    printf("X degerini giriniz:\n");
+    scanf("%d", &X);
+    printf("Y degerini giriniz:\n");
+    scanf("%d", &Y);
 
 	printf("************************************************\n");
     printf("* Menu *\n");
@@ -86,14 +93,16 @@ int main() {
 
 	int* degrees = inDegrees(g1);
 
-	removeNodesByinDegree(g1, 1);
+	removeNodesByinDegree(g1, M);
 
-	int* degrees2 = inDegrees(g1);
+	int* degreesAfterDeleted = inDegrees(g1);
+
+    printGraph(g1);
 
     switch (deger)
     {
     case 1:
-        printInfluencers(g1, 2, 4, people);
+        printInfluencers(g1, X, Y, people);
         break;
     case 2:
         printf("BASLANGIC DURUMUNDA INDEGREE DEGERLERI\n");
@@ -106,17 +115,18 @@ int main() {
         // print the in-degree of each vertex
         for (int i = 1; i < g1->numNodes; i++)
         {
-            printf("Node %d has in-degree %d\n", i, degrees2[i]);
+            printPersonNameById(people[i-1].id, people, g1->numNodes);
+            printf("Node %d in-degree %d\n", i, degreesAfterDeleted[i]);
         }
 
         for (int i = 1; i < g1->numNodes; i++)
         {
-            if (calculateIndegreeConnectionsBFS(g1, i) == 0)
-                printf("Node %d has %d total connections (DELETED)\n", i, calculateIndegreeConnectionsBFS(g1, i));
+            if (calculateTotalConnectionsBFS(g1, i) == 0)
+                printf("Node %d has %d total connections (DELETED)\n", i, calculateTotalConnectionsBFS(g1, i));
             else
-                printf("Node %d has %d total connections\n", i, calculateIndegreeConnectionsBFS(g1, i));
+                printf("Node %d has %d total connections\n", i, calculateTotalConnectionsBFS(g1, i));
         }
-        printInfluencers(g1, 2, 4, people);
+        printInfluencers(g1, X, Y, people);
         break;
     default:
         break;
@@ -127,6 +137,11 @@ int main() {
 	return 0;
 }
 
+/**
+ * @params The number of nodes in the graph
+ * @returns graph*
+ * @desc Creates a graph with the given number of nodes.
+ */
 graph *createGraph(int numNodes) {
 	graph *g = malloc(sizeof(*g));
     g->persons = malloc(numNodes * sizeof(Person));
@@ -152,6 +167,11 @@ graph *createGraph(int numNodes) {
 	return g;
 }
 
+/**
+ * @params A pointer to the graph to be destroyed
+ * @returns void
+ * @desc Destroys the given graph, freeing all memory associated with it.
+ */
 void destroyGraph(graph* g){
 	int i;
 	if (g->edges == NULL) {
@@ -167,6 +187,11 @@ void destroyGraph(graph* g){
 	free(g);
 }
 
+/**
+ * @params A pointer to the graph
+ * @returns void
+ * @desc Prints the given graph in the GraphViz DOT format.
+ */
 void printGraph(graph *g) {
 	int from, to;
 	printf("graph {\n");
@@ -181,6 +206,11 @@ void printGraph(graph *g) {
 	printf("*}\n");
 }
 
+/**
+ * @params A pointer to the graph, source, destination
+ * @returns bool
+ * @desc Adds an edge between two nodes in a graph.
+ */
 bool addEdge(graph *g, int fromNode, int toNode) {
 	assert(g != NULL);
 	assert(fromNode < g->numNodes);
@@ -193,6 +223,11 @@ bool addEdge(graph *g, int fromNode, int toNode) {
 	return true;
 }
 
+/**
+ * @param A pointer to the graph. source, destination
+ * @returns bool True if the edge exists, false otherwise.
+ * @desc Checks if an edge exists between two nodes in a graph.
+ */
 bool hasEdge(graph *g, int fromNode, int toNode) {
 	assert(g != NULL);
 	assert(fromNode < g->numNodes);
@@ -201,7 +236,11 @@ bool hasEdge(graph *g, int fromNode, int toNode) {
 	return g->edges[fromNode][toNode];
 }
 
-// return the in-degree of a given vertex
+/**
+ * @param pointer to the graph, index of the vertex.
+ * @returns int The in-degree of the vertex.
+ * @desc Returns the in-degree of a given vertex in a graph.
+ */
 int inDegree(graph* G, int u) {
     int degree = 0;
     for (int i = 0; i < G->numNodes; i++) {
@@ -212,7 +251,11 @@ int inDegree(graph* G, int u) {
     return degree;
 }
 
-// return an array of the in-degree of each vertex
+/**
+ * @param A pointer to the graph.
+ * @returns: in-degrees of the vertices.
+ * @desc: Returns an array of the in-degrees of each vertex in a graph.
+ */
 int* inDegrees(graph* g) {
     int* degrees = malloc(sizeof(int)*g->numNodes);
     for (int i = 0; i < g->numNodes; i++) {
@@ -221,10 +264,12 @@ int* inDegrees(graph* g) {
     return degrees;
 }
 
-
-// Function to remove a node from a graph
+/**
+ * @param pointer to the graph, index of the node to remove.
+ * @returns void
+ * @desc: Removes a node from a graph.
+ */
 void remove_node(graph *g, int u) {
-	// Set all elements in the row and column of the adjacency matrix for vertex u to 0
 	int i;
 	for (i = 0; i < g->numNodes; i++) {
 		g->edges[u][i] = false;
@@ -232,7 +277,12 @@ void remove_node(graph *g, int u) {
 	}
 }
 
-// Function to remove nodes from a graph based on their in-degree
+/**
+ * @params: graph* g - a pointer to a graph structure
+ *          int m - the minimum in-degree for a node to be removed
+ * @returns: void
+ * @desc: Removes nodes from the given graph that have an in-degree less than m.
+ */
 void removeNodesByinDegree(graph *g, int m) {
 	int i;
 	for (int u = 0; u < g->numNodes; u++) {
@@ -249,19 +299,35 @@ void removeNodesByinDegree(graph *g, int m) {
 	}
 }
 
-
+/**
+ * @params: graph* g - a pointer to a graph structure
+ *          int x - the minimum in-degree for an influencer node
+ *          int y - the minimum number of total connections for an influencer node
+ *          Person* p - an array of Person structures
+ * @returns: void
+ * @desc: Prints the names and information about influencer nodes in the given graph.
+ */
 void printInfluencers(graph * g, int x, int y, Person * p) {
     int i;
 
 	printf("Influencer nodes:\n");
 
 	for (i = 0; i < g -> numNodes; i++) {
-		if (inDegree(g, i) >= x && calculateIndegreeConnectionsBFS(g, i) >= y) {
-		printf("Node %d: name: %s surname: %s in-degree: %d, total connections: %d\n", i, p[i-1].name, p[i-1].last_name, inDegree(g, i), calculateIndegreeConnectionsBFS(g, i));
+		if (inDegree(g, i) >= x && calculateTotalConnectionsBFS(g, i) >= y) {
+            printPersonNameById(p[i-1].id, p, g -> numNodes);
+		    printf("Node %d: in-degree: %d, total connections: %d\n", i, inDegree(g, i), calculateTotalConnectionsBFS(g, i));
 		}
 	}
 }
 
+/**
+ * @params: const char* data - a string containing data to be parsed
+ *          int* num_people - a pointer to an integer to store the number of people
+ * @returns: Person* - a pointer to an array of Person structures
+ * @desc: Parses a string containing data about people and their connections and
+ *        returns an array of Person structures. The number of people is stored in
+ *        the integer pointed to by num_people.
+ */
 Person* parseData(const char* data, int* num_people) {
     // Split the data into lines
     char* lines[MAX_LINE_LEN];
@@ -298,6 +364,12 @@ Person* parseData(const char* data, int* num_people) {
     return people;
 }
 
+/**
+ * @params: Person* people - an array of Person structures
+ *          int num_people - the number of people in the array
+ * @returns: void
+ * @desc: Frees the memory allocated for the connections array 
+ */
 void freePeople(Person* people, int num_people) {
     for (int i = 0; i < num_people; i++) {
         free(people[i].connections);
@@ -305,7 +377,15 @@ void freePeople(Person* people, int num_people) {
     free(people);
 }
 
-/*breadth first search to calculate the total number of direct and indirect connections*/
+/**
+ * @params: graph* graph - a pointer to a graph structure
+ *          int start - the starting node for the search
+ *          int end - the ending node to search for
+ * @returns: int - the number of connections from the start node to the end node
+ * @desc: Calculates the number of connections from the start node to the end node
+ *        in the given graph using BFS.
+ */
+
 int calculateIndegreeConnections(graph* graph, int start, int end) {
     int queue[MAX_NODES];
     int head = 0;
@@ -343,8 +423,14 @@ int calculateIndegreeConnections(graph* graph, int start, int end) {
 }
 
 
-
-int calculateIndegreeConnectionsBFS(graph* graph, int node) {
+/**
+ * @params: graph* graph - a pointer to a graph structure
+ *          int node - the node to count connections to
+ * @returns: int - the total number of connections to the given node
+ * @desc: Calculates the total number of connections to the given node in the graph
+ *        using BFS.
+ */
+int calculateTotalConnectionsBFS(graph* graph, int node) {
   int connections = 0;
   
   // Iterate through the graph and count the number of connections 
@@ -364,10 +450,17 @@ int calculateIndegreeConnectionsBFS(graph* graph, int node) {
   return connections;
 }
 
+/**
+ * @params: int id - the ID of the person to be searched for
+ *          Person* people - an array of Person structures
+ *          int numPeople - the number of people in the array
+ * @returns: void
+ * @desc: Prints the name of the person with the given ID, if found
+ */
 void printPersonNameById(int id, Person *people, int numPeople) {
     for (int i = 0; i < numPeople; i++) {
         if (people[i].id == id) {
-            printf("Name: %s\n", people[i].name);
+            printf("Name: %s %s \t", people[i].name, people[i].last_name);
             return;
         }
     }
